6246daae153b5063e6d11563a2f98d6f6e507916
Sameer Kanakia <skanakia1@gmail.com>
2018-03-21 20:26:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
